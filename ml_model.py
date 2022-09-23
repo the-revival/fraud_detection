cacc9b8166d8b694dd62b0473821d1bda79cf39a
the-revival <contact.tuhin.patra@gmail.com>
2022-09-23 13:04:49 +0530

    Test commit

--- ml_model.py
+++ ml_model.py
@@ -1,7 +1,7 @@
 print("adding all ML related code here.")
 
 def model1():
-    print("Adding code for ML model1")
+    print("Adding code for ML model1.")
 
 def model2():
     print("Adding code for ML model2")
