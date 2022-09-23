b912f49914e9e85261cd598234e3df189746ba5e
the-revival <contact.tuhin.patra@gmail.com>
2022-09-23 12:18:20 +0530

    Added 3 new ML model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("adding all ML related code here.")
+
+def model1():
+    print("Adding code for ML model1")
+
+def model2():
+    print("Adding code for ML model2")
+
+def model3():
+    print("Adding code for ML model3")
