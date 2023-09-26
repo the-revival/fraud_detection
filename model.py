773d2eab83420756e965963080973e7736a37bb1
the-revival <contact.tuhin.patra@gmail.com>
2023-09-26 21:52:45 +0530

    Added a new print statement after merging model with master

--- model.py
+++ model.py
@@ -10,3 +10,5 @@ def model3():
     print('This is the third model')
 
 print('Model 2 performs the best and is the final model')
+
+print('This print statement is added after merging the model branch with master')
