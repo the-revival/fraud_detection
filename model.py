edd58d7dd8ef35afc4dc4850a286880b695cebb2
the-revival <contact.tuhin.patra@gmail.com>
2023-09-26 20:36:32 +0530

    Added 3 models to compare the performance

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print('Test test')
+
+def model1():
+    print('This is the first model')
+
+def model2():
+    print('This is the second model')
+
+def model3():
+    print('This is the third model')
